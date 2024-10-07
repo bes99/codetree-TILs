3f69255ab5a9345e63da58784c0d445c0ccda6c0
변은성 <86958447+bes99@users.noreply.github.com>
2024-10-07 15:19:18 +0900

    Title: 팩맨, Time: 1053ms, Memory: 2MB, Status: Time Limit Exceed - CodeTree

--- 241007/팩맨/pacman.cpp
+++ 241007/팩맨/pacman.cpp
@@ -4,10 +4,9 @@
 #include <utility>
 #include <tuple>
 #include <queue>
-#include <map>
 using namespace std;
 // 몬스터,알, 시체, 턴
-pair<pair<int,int>,pair<int,int>> arr[5][5],cpy[5][5];
+pair<pair<int,int>,pair<int,int>> arr[5][5];
 int m,t,max_sum;
 pair<int,int> pacman;
 // r,c,d,state
@@ -17,16 +16,14 @@ int dx[8] = {0,-1,-1,-1,0,1,1,1};
 int dr[4] = {-1,0,1,0};
 int dc[4] = {0,-1,0,1};
 vector<int> dir,last;
-map<tuple<int,int,int>,int> mons,cmons;
-queue<pair<pair<int,int>, pair<int,int>>> q;
-void copy(){
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            cpy[i][j].first.first = arr[i][j].first.first;
-            cpy[i][j].first.second = arr[i][j].first.second;
-            cpy[i][j].second.first = arr[i][j].second.first;
-            cpy[i][j].second.second = arr[i][j].second.second;
-        }
+vector<tuple<int,int,int,int>> tp;
+// 팩맨 이동 체크 로직
+queue<pair<pair<int,int>,int>> q;
+bool compare(tuple<int,int,int,int> &a, tuple<int,int,int,int> &b){
+    if(get<0>(a) != get<0>(b))
+        return get<0>(a) < get<0>(b);
+    else{
+        return get<1>(a) < get<1>(b);
     }
 }
 void mv(int row, int col){
@@ -79,6 +76,7 @@ void mv(int row, int col){
 // 펙맨 이동과 시체 업데이트
 void update_died(){
     //cout<<last.size()<<"\n";
+    sort(tp.begin(),tp.end(),compare);
     for(int i=0;i<last.size();i++){
         pacman.first+=dr[last[i]];
         pacman.second+=dc[last[i]];
@@ -86,8 +84,16 @@ void update_died(){
         int r = pacman.first;
         int c = pacman.second;
         //cout<<r<<" "<<c<<"\n";
-        for(int j=0;j<8;j++){
-            mons[{r,c,j}]=0;
+        for(int j=tp.size()-1;j>=0;j--){
+            int tr = get<0>(tp[j]);
+            int tc = get<1>(tp[j]);
+            int state = get<3>(tp[j]);
+            //cout<<tr<<" "<<tc<<"\n";
+            if(tr==r && tc==c && state==1){
+                tp.erase(tp.begin()+j);
+            }
+            else if(tr==r && tc<c) break;
+            else if (tr<r && tc==c) break;
         }
         if(arr[r][c].first.first>0){
             arr[r][c].second.first += arr[r][c].first.first;
@@ -111,57 +117,54 @@ void egg(){
     while(!q.empty()){
         int r = q.front().first.first;
         int c = q.front().first.second;
-        int dir = q.front().second.first;
-        int cnt = q.front().second.second;
-        //cout<<r<<" "<<c<<" "<<dir<<" "<<cnt<<"\n";
+        int dir = q.front().second;
         q.pop();
-        mons[{r,c,dir}]+=cnt;
-        arr[r][c].first.first+=cnt;
-    }
-}
-void move_mons(int r, int c){
-    for(int d=0;d<8;d++){
-        if(cmons[{r,c,d}]>0){
-            int td = d;
-            int tmp = cmons[{r,c,d}];
-            q.push({{r,c},{d,tmp}});
-            //cout<<r<<" "<<c<<" "<<d<<" "<<tmp<<"\n";
-            int cnt=8;
-            while(cnt--){
-                int rr = r+dy[td];
-                int cc = c+dx[td];
-                if(rr<0 || rr>=4 || cc<0 || cc>=4 || (rr == pacman.first && cc==pacman.second) || cpy[rr][cc].second.first>0){
-                    td+=1;
-                    if(td==8) td = 0;
-                }
-                else{
-                    mons[{r,c,d}] = 0;
-                    arr[r][c].first.first-=tmp;
-                    mons[{rr,cc,td}] += tmp;
-                    arr[rr][cc].first.first += tmp;
-                    break;
-                }
-            }
-        }
+        tp.push_back({r,c,dir,1});
+        arr[r][c].first.first++;
     }
 }
 void simulate(){
     for(int i=0;i<t;i++){
-        copy();
-        cmons = mons;
-        for(int r=0;r<4;r++){
-            for(int c=0;c<4;c++){
-                if(cpy[r][c].first.first>0){
-                    move_mons(r,c);
+        int size=tp.size();
+        max_sum=-1;
+        //cout<<size<<"\n";
+        for(int s=0;s<size;s++){
+            int r = get<0>(tp[s]);
+            int c = get<1>(tp[s]);
+            int d = get<2>(tp[s]);
+            int state = get<3>(tp[s]);
+            //cout<<r<<" "<<c<<" "<<d<<" "<<state<<"\n";
+            if(state==1){
+                // 몬스터 알 낳기
+                q.push({{r,c},d});
+
+                // 몬스터 이동
+                int cnt=8;
+                while(cnt--){
+                    int rr = r+dy[d];
+                    int cc = c+dx[d];
+                    if(rr<0 || rr>=4 || cc<0 || cc>=4 || (rr == pacman.first && cc==pacman.second) || arr[rr][cc].second.first>0){
+                        d+=1;
+                        if(d==8) d = 0;
+                    }
+                    else{
+                        get<0>(tp[s]) = rr;
+                        get<1>(tp[s]) = cc;
+                        get<2>(tp[s]) = d;
+                        arr[rr][cc].first.first += 1;
+                        arr[r][c].first.first--;
+                        break;
+                    }
                 }
+                
             }
         }
         // 팩맨 이동
-        //cout<<pacman.first<<" "<<pacman.second<<"\n";
         mv(pacman.first,pacman.second);
         update_died();
         last.clear();
         egg();
+        //cout<<pacman.first<<" "<<pacman.second<<"\n";
     }
 }
 int print_ans(){
@@ -183,13 +186,19 @@ int main() {
     for(int i=0;i<m;i++){
         cin>>a>>b>>c;
         a--,b--,c--;
-        mons[{a,b,c}]++;
+        tp.push_back({a,b,c,1});
         arr[a][b].first.first++;
         arr[a][b].first.second=0;
         arr[a][b].second.first=0;
         arr[a][b].second.second=0;
     }
+
     simulate();
+    // sort(tp.begin(),tp.end(),compare);
+    // for(int i=0;i<tp.size();i++){
+
+    //     cout<<get<0>(tp[i])<<" "<<get<1>(tp[i])<<" "<<get<2>(tp[i])<<" "<<get<3>(tp[i])<<"\n";
+    // }
 
     // for(int i=0;i<4;i++){
     //     for(int j=0;j<4;j++){
