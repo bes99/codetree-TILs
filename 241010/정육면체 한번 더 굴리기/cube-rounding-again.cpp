07a5be01f019d0131a9bd251ef472d2ca2d2f8b9
변은성 <86958447+bes99@users.noreply.github.com>
2024-10-10 15:49:11 +0900

    Title: 정육면체 한번 더 굴리기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241010/정육면체 한번 더 굴리기/cube-rounding-again.cpp
+++ 241010/정육면체 한번 더 굴리기/cube-rounding-again.cpp
@@ -8,19 +8,19 @@ int arr[21][21];
 // 우,하,좌,상
 int dr[4] = {0,1,0,-1};
 int dc[4] = {1,0,-1,0};
-int dv=6;
 int row=0,col=0;
 int u=1,f=2,r=3;
 int n,m,d,ans;
 bool visited[21][21];
 int bfs(int dy, int dx){
-    int cnt=1;
+    int sum=0;
     queue<pair<int,int>> q;
     q.push({dy,dx});
     visited[dy][dx] = true;
     while(!q.empty()){
         int r = q.front().first;
         int c = q.front().second;
+        sum += arr[r][c];
         q.pop();
         for(int i=0;i<4;i++){
             int rr = r+dr[i];
@@ -29,41 +29,29 @@ int bfs(int dy, int dx){
             if(!visited[rr][cc] && arr[rr][cc]==arr[dy][dx]){
                 visited[rr][cc] = true;
                 q.push({rr,cc});
-                cnt++;
             }
         }
     }
     for(int i=0;i<n;i++){
         fill(visited[i],visited[i]+n,false);
     }
-    return cnt*arr[dy][dx];
+    return sum;
 }
 void simulate(){
     for(int i=0;i<m;i++){
-        int r = row + dr[d];
-        int c = col + dc[d];
+        int rr = row + dr[d];
+        int cc = col + dc[d];
         // 격자 밖으로 나갔을 때 반대로 나가는 로직
-        if(r<0){
-            r=1;
-            d=1;
-        }
-        else if(r>=n){
-            r = n-2;
-            d=3;
-        }
-        else if(c<0){
-            c = 1;
-            d=0;
-        }
-        else if(c>=n){
-            c = n-2;
-            d=2;
+        if(rr<0 || rr>=n || cc<0 || cc>=n){
+            d = d<2 ? d+2 : d-2;
+            rr += dr[d];
+            cc += dc[d];
         }
 
-        row = r;
-        col = c;
+        row = rr;
+        col = cc;
         ans += bfs(row,col);
-        
+        //cout <<row<<" "<<col<<"\n";
         if(d==0){
             tie(u,f,r) = make_tuple(7-r,f,u);  
         }
@@ -76,7 +64,6 @@ void simulate(){
         else if(d==3){
             tie(u,f,r) = make_tuple(f,7-u,r);
         }
-
         int down = 7-u; 
 
         if(down>arr[row][col]){
