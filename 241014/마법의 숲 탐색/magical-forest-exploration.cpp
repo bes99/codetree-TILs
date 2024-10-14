c4dcf01dedbe037589cda3be2c5afce2073b8157
변은성 <86958447+bes99@users.noreply.github.com>
2024-10-14 13:17:52 +0900

    Title: 마법의 숲 탐색, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241014/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 241014/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -8,8 +8,9 @@ int dr[4]={-1,0,1,0};
 int dc[4]={0,1,0,-1};
 //  행,열,출구 방향
 vector<tuple<int,int,int>> vec;
-bool checked[72][72];
-bool visited[72][72];
+bool visited[73][73];
+bool checked[73][73];
+int dp[73][73];
 // 아래로 이동 가능한지 여부
 bool is_down(int row, int col){
     int dy[3] = {1,2,1};
@@ -33,7 +34,7 @@ bool is_left(int row, int col){
     for(int d=0;d<5;d++){
         int r = row + dy[d];
         int c = col + dx[d];
-        if(checked[r][c] || r<0 || r>=R || c<0 || c>=C){
+        if(checked[r][c]  || r<0 || r>=R || c<0 || c>=C){
             return false;
         }
     }
@@ -49,7 +50,7 @@ bool is_right(int row, int col){
     for(int d=0;d<5;d++){
         int r = row + dy[d];
         int c = col + dx[d];
-        if(checked[r][c] || r<0 || r>=R || c<0 || c>=C){
+        if(checked[r][c]  || r<0 || r>=R || c<0 || c>=C){
             return false;
         }
     }
@@ -59,45 +60,60 @@ void right(int idx){
     get<0>(vec[idx])++;
     get<1>(vec[idx])++;
 }
-void lacate(int idx){
+void lacate(int idx, int max_row){
     int r = get<0>(vec[idx]);
     int c = get<1>(vec[idx]);
     for(int i=0;i<4;i++){
         int rr = r+dr[i];
         int cc = c+dc[i];
+        dp[rr][cc] = max_row;
         checked[rr][cc] = true;
     }
 }
 void clear_map(){
-    for(int i=0;i<R+2;i++){
-        for(int j=0;j<C+2;j++){
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            dp[i][j] = 0;
             checked[i][j] = false;
         }
     }
 }
-int bfs(int row, int col){
-    for(int i=0;i<R;i++){
-        fill(visited[i],visited[i]+C,false);
-    }
-    int max_row = 0;
-    queue<pair<int,int>> q;
-    q.push({row,col});
-    visited[row][col] = true;
-    while(!q.empty()){
-        int r = q.front().first;
-        int c = q.front().second;
-        q.pop();
-        max_row = max(max_row,r);
-        for(int i=0;i<4;i++){
-            int rr = r+dr[i];
-            int cc = c+dc[i];
-            if(!visited[rr][cc] && checked[rr][cc]){
-                q.push({rr,cc});
-                visited[rr][cc] = true;
-            }
+int bfs(int row, int col, int mr){
+    int max_row = mr;
+    bool flag = false;
+    for(int i=0;i<4;i++){
+        int r = row+dr[i];
+        int c = col+dc[i];
+        if(checked[r][c]){
+            flag =true;
+            max_row = max(max_row, dp[r][c]);
         }
     }
-    return max_row;
+    if(flag) return max_row;
+
+    // for(int i=0;i<R;i++){
+    //     fill(visited[i],visited[i]+C,false);
+    // }
+    // queue<pair<int,int>> q;
+    // q.push({row,col});
+    // visited[row][col] = true;
+    // checked[row][col] = true;
+    // while(!q.empty()){
+    //     int r = q.front().first;
+    //     int c = q.front().second;
+    //     q.pop();
+    //     max_row = max(max_row,r);
+    //     for(int i=0;i<4;i++){
+    //         int rr = r+dr[i];
+    //         int cc = c+dc[i];
+    //         if(!visited[rr][cc] && !checked[rr][cc]){
+    //             q.push({rr,cc});
+    //             visited[rr][cc] = true;
+    //             checked[rr][cc] = true;
+    //         }
+    //     }
+    // }
+    // return max_row;
 }
 
 void simulate(){
@@ -120,14 +136,15 @@ void simulate(){
             }
             else break;
         }
-        if(get<0>(vec[i])<2) {
+        if(get<0>(vec[i])<3) {
             clear_map();
             continue;
         }
-        lacate(i);
-        // 그래프 탐색 로직 추가
         int d = get<2>(vec[i]);
-        int max_row = max(get<0>(vec[i]), bfs(get<0>(vec[i])+dr[d], get<1>(vec[i])+dc[d]));
+        int max_row = bfs(get<0>(vec[i])+dr[d], get<1>(vec[i])+dc[d], get<0>(vec[i]));
+        lacate(i,max_row);
+        dp[get<0>(vec[i])][get<1>(vec[i])] = max_row;
+
         ans += max_row;
     }
 }
